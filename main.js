0f24338b10e7ffb257788ae0586f8e63ab3797e9
Fizal Sarif <fizal.sarif@gmail.com>
2019-12-08 02:52:27 -0500

    finish update script

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ let packages = {};
 
 const render = () => {
   tbody.innerHTML = '';
-
   const packageKeys = Object.keys(packages);
-
   packageKeys.reduce((p,c,i) => {
     if (packages[c].received) {
       return p.concat([c]);
@@ -19,14 +17,15 @@ const render = () => {
     tbody.innerHTML += `
 
     <tr>
+      <td>${package.note}</td>
       <td>${package.tracking_number}</td>
       <td>${package.carrier}</td>
       <td>${package.status || '<img src="https://weeve.network/images/Loader.gif" height="40px">'}</td>
       <td>
         <button
-          class="btn btn-sm btn-${package.received ? 'success' : 'danger'} btn-received"
+          class="btn btn-sm btn-${package.received ? 'success' : 'primary'} btn-received"
           data-tracking_number="${package.tracking_number}">
-          ${package.received ? 'Received' : 'Not Received'}
+          ${package.received ? 'Received' : 'Mark Received'}
           </button>
         <button
           class="btn btn-sm btn-danger btn-delete" data-tracking_number="${package.tracking_number}">
@@ -39,12 +38,12 @@ const render = () => {
   });
 }
 
-const update = async () => {
+const update = async (coll) => {
   console.log("UPDATE");
   packages = {};
 
   try {
-    const collection = await db.collection("tracking_checklist").get();
+    const collection = coll ? coll.docs : await db.collection("tracking_checklist").get();
     collection.forEach(packageRef => {
       const package = packageRef.data();
       packages[package.tracking_number] = package;
@@ -60,12 +59,12 @@ form.onsubmit = async (e) => {
   e.preventDefault();
   const data = {
     tracking_number: e.target.trackingno.value,
+    note: e.target.note.value,
     carrier: e.target.carrier.value,
     received: false
   }
 
   if (data.tracking_number == '') return 0;
-
 
   console.log(data);
 
@@ -77,14 +76,18 @@ form.onsubmit = async (e) => {
 
 
   e.target.trackingno.value = '';
-  update();
 }
-
-update();
 
 tbody.addEventListener('click', e =>{
   if (e.target.classList.contains('btn-received')) {
-
+    db.collection("tracking_checklist")
+    .doc(e.target.dataset.tracking_number)
+    .set({
+      ...packages[e.target.dataset.tracking_number],
+      received: !packages[e.target.dataset.tracking_number].received
+    })
+    .then(console.log)
+    .catch(console.error);
   }
 
   if (e.target.classList.contains('btn-delete')) {
@@ -92,10 +95,12 @@ tbody.addEventListener('click', e =>{
     db.collection("tracking_checklist")
       .doc(e.target.dataset.tracking_number)
       .delete()
-      .then(update)
+      .then(console.log)
       .catch(console.error);
 
   }
 
 });
 
+db.collection("tracking_checklist")
+  .onSnapshot(update);
---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+require('dotenv').config();
+const admin = require('firebase-admin');
+const axios = require('axios');
+const sleep = require('await-sleep');
+
+const serviceAccount = require("./admin.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://general-open-project.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+const main = async () => {
+  const tracking_checklist = await db.collection('tracking_checklist').get();
+  tracking_checklist.forEach(async doc =>{
+    const data = doc.data();
+    const req_url = `https://api.shipengine.com/v1/tracking?carrier_code=${data.carrier.toLowerCase()}&tracking_number=${data.tracking_number}`;
+
+    if (data.received) return 0;
+
+    console.log('getting status of ' + data.tracking_number);
+
+    const tracking_req = await axios.get(req_url, {
+      headers: {
+        'API-Key': process.env.TRACKING_TOKEN
+      }
+    });
+
+    data['status'] = tracking_req.data.status_description;
+    console.log('saving ' + data.tracking_number);
+    await db.collection('tracking_checklist').doc(data.tracking_number).set(data);
+    await sleep(1000);
+  });
+}
+
+main();
